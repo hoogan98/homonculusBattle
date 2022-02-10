e0c3814c665650f6be6134a08de2537cbc6fb3f0
hoogan98 <grant.hartman001@gmail.com>
2022-02-10 08:41:25 -0500

    fixed default z-indexes

--- Assets/Scripts/Parts/Bone.cs
+++ Assets/Scripts/Parts/Bone.cs
@@ -14,7 +14,7 @@ public class Bone : Part
         ratio = r;
         _drawAnchor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 start = new Vector3(_drawAnchor.x + (GetComponent<Renderer>().bounds.size.x / 2), _drawAnchor.y, 1);
+        Vector3 start = new Vector3(_drawAnchor.x + (GetComponent<Renderer>().bounds.size.x / 2), _drawAnchor.y, 2);
 
         transform.position = start;
     }
