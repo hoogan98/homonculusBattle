f3299bb1aef898d288dc3f57eb6b024527dd8232
hoogan98 <grant.hartman001@gmail.com>
2022-08-03 15:17:34 -0400

    updated all scenes to have brightness filter and made it work

---
+++ Assets/Scripts/BrightnessLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class BrightnessLoader : MonoBehaviour
+{
+    public PostProcessProfile brightnessFilter;
+
+    public void Start()
+    {
+        brightnessFilter.GetSetting<ColorGrading>().postExposure.Override(Settings.instance.brightness);
+    }
+}
--- Assets/Scripts/UI/BrightnessSliderHandler.cs
+++ Assets/Scripts/UI/BrightnessSliderHandler.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class BrightnessSliderHandler : MonoBehaviour
 {
     public Slider brightnessSlider;
+    public PostProcessProfile brightnessFilter;
 
     public void Start() {
         brightnessSlider.value = Settings.instance.brightness;
@@ -14,5 +16,6 @@ public class BrightnessSliderHandler : MonoBehaviour
     public void OnSliderChange()
     {
         Settings.instance.brightness = brightnessSlider.value;
+        brightnessFilter.GetSetting<ColorGrading>().postExposure.Override(brightnessSlider.value);
     }
 }
