1ec4ef2c9b3679888f572352464430539fafd442
hoogan98 <grant.hartman001@gmail.com>
2022-10-19 15:33:40 -0400

    gibs exist now and reset the scene on a timer

--- Assets/Scripts/Giblet.cs
+++ Assets/Scripts/Giblet.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Giblet : MonoBehaviour
 {
     public Sprite[] possibleSprites;
     public float velDeviation;
     public float torqueDeviation;
+    public float resetSeconds;
+
 
     void Start()
     {
@@ -15,5 +19,15 @@ public class Giblet : MonoBehaviour
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.AddForce(new Vector2(Random.Range(-velDeviation, velDeviation), Random.Range(-velDeviation, velDeviation)));
         rb.AddTorque(Random.Range(-torqueDeviation, torqueDeviation));
+
+        StartCoroutine(ResetAfterSeconds());
+    }
+
+    IEnumerator ResetAfterSeconds() {
+        yield return new WaitForSeconds(resetSeconds);
+
+        Debug.Log("running scene reset");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Destroy(gameObject);
     }
 }
