75d5d50aad11220e5bf30e41a8d52003520caab7
hoogan98 <grant.hartman001@gmail.com>
2022-10-20 16:25:36 -0400

    they make sounds now

--- Assets/Scripts/Giblet.cs
+++ Assets/Scripts/Giblet.cs
@@ -10,7 +10,9 @@ public class Giblet : MonoBehaviour
     public float velDeviation;
     public float torqueDeviation;
     public float resetSeconds;
+    public AudioClip[] hitSounds;
 
+    private AudioSource player;
 
     void Start()
     {
@@ -20,14 +22,32 @@ public class Giblet : MonoBehaviour
         rb.AddForce(new Vector2(Random.Range(-velDeviation, velDeviation), Random.Range(-velDeviation, velDeviation)));
         rb.AddTorque(Random.Range(-torqueDeviation, torqueDeviation));
 
+        player = GetComponent<AudioSource>();
+
         StartCoroutine(ResetAfterSeconds());
     }
 
-    IEnumerator ResetAfterSeconds() {
+    IEnumerator ResetAfterSeconds()
+    {
         yield return new WaitForSeconds(resetSeconds);
 
         Debug.Log("running scene reset");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Giblet")
+        {
+            return;
+        }
+
+        if (!player.isPlaying)
+        {
+            player.clip = hitSounds.PickRandom<AudioClip>();
+            player.Play();
+        }
     }
 }
